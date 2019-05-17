db52957f1dfc8af1b47eb938b65c75573b068658
Stawbewwy <46510239+Stawbewwy@users.noreply.github.com>
2019-05-16 23:57:55 -0700

    Cleanup, documentation

--- W_Application_Closer/W_Application_Closer/W_Application_Closer.cpp
+++ W_Application_Closer/W_Application_Closer/W_Application_Closer.cpp
@@ -7,15 +7,18 @@
 
 BOOL CALLBACK EnumWindowsProc(HWND hWnd, long lParam) {
 	//char buff[255];
+
+	//TCHAR actually stands for "Type Char". Able to convert itself to either the regular old ASCII charset (char), or into unicode charset (wchar) 
+	//This will be used for a wchar (unicode char)
 	TCHAR wndTitle[300];
 
+	//With the window @hWnd, if it's a visible process, we want to print it out.
 	if (IsWindowVisible(hWnd)) {
 		GetWindowText(hWnd, (LPWSTR)wndTitle, 254);
 
-		//MessageBox(NULL, wndTitle,NULL, MB_OK);
+		//cout does NOT work as desired with unicode set, so we use wcout to work properly with unicode strings (wchar strings).
 		std::wcout << wndTitle << std::endl;
-		//printf("%s\n", wndTitle);
-		//system("pause");
+
 	}
 
 	return TRUE;
